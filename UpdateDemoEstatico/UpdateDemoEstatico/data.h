61a184e27f19f4cf9f5ad62cd4723975d08cdb9d
KevinAlcalde <54211993+KevinAlcalde@users.noreply.github.com>
2025-05-24 16:43:20 -0300

    Update data.h

--- UpdateDemoEstatico/UpdateDemoEstatico/data.h
+++ UpdateDemoEstatico/UpdateDemoEstatico/data.h
@@ -1,4 +1,4 @@
 const char* nombre = "esp-device";
-const char* ssid    = "NOAH";
-const char* password = "21554627R";
+const char* ssid    = "TuWifi";
+const char* password = "tuClave";
 String Pagina = R"====(<form method='POST' action='/actualizar' enctype='multipart/form-data'><input type='file' name='update'><br><input type='submit' value='actualizar'></form>)====";
