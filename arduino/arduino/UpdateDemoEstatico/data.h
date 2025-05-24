668733fa62a5a0d053944be4f03fd8d36054694f
KevinAlcalde <54211993+KevinAlcalde@users.noreply.github.com>
2025-05-24 16:41:31 -0300

    Delete arduino/arduino directory

--- arduino/arduino/UpdateDemoEstatico/data.h
+++
@@ -1,3 +0,0 @@
-const char* ssid    = "ALSW_ESTUDIO";
-const char* password = "Fullpower7##";
-String Pagina = R"====(<form method='POST' action='/actualizar' enctype='multipart/form-data'><input type='file' name='update'><br><input type='submit' value='actualizar'></form>)====";
